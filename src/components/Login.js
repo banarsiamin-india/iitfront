c0666fff1a62ed50d8177dd255bb74f27e7a1d1e
amin khan <amin.k@indiainfotech.com>
2024-11-07 19:54:58 +0530

    add new login changes

--- src/components/Login.js
+++ src/components/Login.js
@@ -36,12 +36,12 @@ const Login = () => {
     </Button> */}
     
           <div className="mb-3">
-            <label className="form-label">Email</label>
-            <input type="email" className="form-control" value={email} onChange={(e) => setEmail(e.target.value)} />
+            <label className="form-label">Email *</label>
+            <input type="email" className="form-control" value={email} onChange={(e) => setEmail(e.target.value)} required />
           </div>
           <div className="mb-3">
-            <label className="form-label">Password</label>
-            <input type="password" className="form-control" value={password} onChange={(e) => setPassword(e.target.value)} />
+            <label className="form-label">Password *</label>
+            <input type="password" className="form-control" value={password} onChange={(e) => setPassword(e.target.value)} required/>
           </div>
           {/* <button type="submit" className="btn btn-primary">Login</button> */}
           <Button type="submit" variant="primary">Login</Button>
